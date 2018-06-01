306d17ff4228f83a832b821f1f4fe9e1c157658b
YuRuipeng <1093476683@qq.com>
2018-06-01 16:33:52 +0800

    ai人工智能图片识别   --加juqery 接口增删改查

--- 知识学习/jquery 增删改查/server/server.js
+++ 知识学习/jquery 增删改查/server/server.js
@@ -1,20 +1,29 @@
 var Http = require("http");
 var fs = require("fs");
 var Url = require("url");
+var queryString = require("querystring");
 
 //req：请求 所有客户端的信息，包括？传参
 //res：响应 返回内容和方法
 //req.url目录加名称加传参的数据
 var server = Http.createServer((req,res)=>{
     let {pathname,query} = Url.parse(req.url,true);//解构赋值
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.setHeader("X-Powered-By",' 3.2.1')
-    res.setHeader('Content-type', 'application/json;charset=utf8');
+    // res.setHeader("Access-Control-Allow-Origin", "*");
+    // res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // res.setHeader("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    // res.setHeader("X-Powered-By",' 3.2.1')
+    // res.setHeader('Content-type', 'application/json;charset=utf8');
     if (req.method == "OPTIONS")
     return res.end();
-
+    let reg = /\.(HTML|CSS|JS|ICO)/i;
+    if (reg.test(pathname)) {
+        try {
+        let con = fs.readFileSync('../' + pathname, 'utf8');
+            res.end(con);
+        } catch (error) {
+            res.end('FILE IS NOT FIND');
+        }
+    }
 
     
     //var list = fs.readFileSync("book.json")     同步读取到数据    操作数据
@@ -28,11 +37,10 @@ var server = Http.createServer((req,res)=>{
             }
         })
     }
-    console.log(pathname)
+    //console.log(pathname)
     switch(pathname){
         case "/getlist":
             //查找用户信息
-            console.log(1)
             if(req.method==="GET"){
                 read(function(list){
                     res.end(JSON.stringify({
@@ -58,8 +66,11 @@ var server = Http.createServer((req,res)=>{
         case "/getInfo":
             //查找单个用户信息
             if(req.method==="GET"){
-
+                //console.log(query)
                 read(function(list){
+                    var arr = list.filter((item,index)=>{
+                        return item.id == query.id
+                    })
                     res.end(JSON.stringify({
                         "status": "success",
                         "result": {
@@ -80,32 +91,120 @@ var server = Http.createServer((req,res)=>{
                 }))
             }
         break;
-        case "addInfo":
+        case "/addInfo":
             //增加用户信息
             if(req.method==="POST"){
-
+                var data = '';
+                req.on('data', function (chunk) {
+                    data += chunk;
+                });
+                req.on('end', function () {
+                    data = decodeURI(data);
+                    var str =  queryString.parse(data);
+                    read(function(list){
+                        str.id = list.length+1
+                        list.push(str)
+                        fs.writeFile("list.json",JSON.stringify(list),function(err){
+                        })
+                        res.end(JSON.stringify({
+                            "status": "success",
+                            "result": {
+                                "code": 10002,
+                                "msg": '返回成功',
+                                "data": list,
+                            }    
+                        }))
+                    })
+                })
             }else{
-                
+                res.end(JSON.stringify({
+                    "status": "fail",
+                    "result": {
+                        "code": 10001,
+                        "msg": '返回失败',
+                        "data": [],
+                    }    
+                }))
             }
         break;
-        case "changeInfo":
+        case "/changeInfo":
             //修改某个用户信息
             //增加用户信息
             if(req.method==="POST"){
-
+                var data = '';
+                req.on('data', function (chunk) {
+                    data += chunk;
+                });
+                req.on('end', function () {
+                    data = decodeURI(data);
+                    var str =  queryString.parse(data);
+                    read(function(list){
+                        list.map((item,index)=>{
+                            if(item.id == str.id){
+                                item.name = str.name
+                                item.sex = str.sex
+                                item.score = str.score
+                            }
+                        })
+                        fs.writeFile("list.json",JSON.stringify(list),function(err){
+                        })
+                        res.end(JSON.stringify({
+                            "status": "success",
+                            "result": {
+                                "code": 10002,
+                                "msg": '返回成功',
+                                "data": list,
+                            }    
+                        }))
+                    })
+                })
             }else{
-                
+                res.end(JSON.stringify({
+                    "status": "fail",
+                    "result": {
+                        "code": 10001,
+                        "msg": '返回失败',
+                        "data": [],
+                    }    
+                }))
             }
         break;
-        case "removeInfo":
+        case "/removeInfo":
             //删除某个用户信息 
             if(req.method==="GET"){
+                //console.log(query)
+                read(function(list){
+                    var arr = list.filter((item,index)=>{
+                        return item.id != query.id
+                    })
+                    arr.map((item,index)=>{
 
+                        item.id=index+1
+                    })
+                    fs.writeFile("list.json",JSON.stringify(arr),function(err){
+
+                    })
+                    res.end(JSON.stringify({
+                        "status": "success",
+                        "result": {
+                            "code": 10002,
+                            "msg": '返回成功',
+                            "data": arr,
+                        }    
+                    }))
+                })
             }else{
-                
+                res.end(JSON.stringify({
+                    "status": "fail",
+                    "result": {
+                        "code": 10001,
+                        "msg": '返回失败',
+                        "data": [],
+                    }    
+                }))
             }
         break;
         
     }
 })
-server.listen(8000);
+server.listen(8088);
